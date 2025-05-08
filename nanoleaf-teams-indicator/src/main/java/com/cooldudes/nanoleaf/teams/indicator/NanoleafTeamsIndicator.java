f14441e36517fe77a41aad4346097e6c498b6a1d
Bryson S <bryson.simmerman@gmail.com>
2025-05-08 19:25:41 -0400

    init project
    init project added azure dependences

---
+++ nanoleaf-teams-indicator/src/main/java/com/cooldudes/nanoleaf/teams/indicator/NanoleafTeamsIndicator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cooldudes.nanoleaf.teams.indicator;
+
+/**
+ *
+ * @author bryson
+ */
+public class NanoleafTeamsIndicator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
